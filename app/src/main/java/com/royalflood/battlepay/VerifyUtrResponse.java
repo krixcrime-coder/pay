b4281ab889fd672e5fc1c3c9b7cbe2c851231070
krixcrime-coder <tumhara-email@gmail.com>
2026-07-30 20:09:51 +0530

    Empty repo via github_sync_tool

--- app/src/main/java/com/royalflood/battlepay/VerifyUtrResponse.java
+++
@@ -1,8 +0,0 @@
-package com.royalflood.battlepay;
-
-public class VerifyUtrResponse {
-    public boolean success;
-    public String error;
-    public String message;
-    public int coins_added;
-}
